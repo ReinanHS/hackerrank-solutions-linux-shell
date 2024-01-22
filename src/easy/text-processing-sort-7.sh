9dab9011fede9e14a8d4db9982da6b924c7c6d8d
Reinan Gabriel <28494067+reinanhs@users.noreply.github.com>
2024-01-22 19:44:54 -0300

    feat: realiza a resolução da atividade do comando sort

---
+++ src/easy/text-processing-sort-7.sh
@@ -0,0 +1,14 @@
+# Você recebe um arquivo de dados meteorológicos delimitados por tubos (TSV). Não há coluna de cabeçalho neste arquivo de dados. 
+# As primeiras cinco colunas desses dados são: 
+# (a) o nome da cidade 
+# (b) a temperatura média mensal em janeiro (em Fahreneit). 
+# (c) a temperatura média mensal em abril (em Fahreneit).
+# (d) a temperatura média mensal em julho (em Fahreneit). 
+# (e) a temperatura média mensal em outubro (em Fahreneit).
+# Você precisa classificar este arquivo em ordem decrescente da segunda coluna (ou seja, a temperatura média mensal em janeiro).
+while IFS= read -r FILE || [ -n "$FILE" ]
+do
+    echo "${FILE}" >> temp_file.txt
+done
+
+cat temp_file.txt | sort -t '|' -k2 -n -r
