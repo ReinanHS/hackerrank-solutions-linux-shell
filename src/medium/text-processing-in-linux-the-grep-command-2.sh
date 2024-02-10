b0cf6ed793be6dac64bbef08e8292311a87de9ad
Reinan Gabriel <28494067+reinanhs@users.noreply.github.com>
2024-02-10 19:16:34 -0300

    feat: realiza a resolução da atividade com grep

---
+++ src/medium/text-processing-in-linux-the-grep-command-2.sh
@@ -0,0 +1,5 @@
+# Você recebe um arquivo de texto que será canalizado para seu comando por meio de STDIN. 
+# Use grep para exibir todas as linhas que contêm a palavra the.
+# A pesquisa NÃO deve ser sensível a maiúsculas e minúsculas.
+# Exiba apenas as linhas do arquivo de entrada que contêm a palavra 'the'.
+grep -i -w "the"
