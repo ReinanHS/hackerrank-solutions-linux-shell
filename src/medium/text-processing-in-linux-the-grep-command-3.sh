e9831606b2b6f1a057f07e981243c3a508cea923
Reinan Gabriel <28494067+reinanhs@users.noreply.github.com>
2024-02-11 14:10:08 -0300

    feat: realiza a resolução da atividade com grep

---
+++ src/medium/text-processing-in-linux-the-grep-command-3.sh
@@ -0,0 +1,4 @@
+# Você recebe um arquivo de texto que será canalizado para seu comando por meio de STDIN. 
+# Use grep para remover todas as linhas que contêm a palavra 'that'. 
+# A pesquisa NÃO deve ser sensível a maiúsculas e minúsculas.
+grep -v -i -w "that"
